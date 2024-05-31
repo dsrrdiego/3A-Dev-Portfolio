c9815ab1024899d3157a3b3446caf7b70735340e
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-05-31 18:18:29 -0300

    Add Hero Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './layout/header/header.component';
+import { HeroComponent } from './layout/hero/hero.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HeroComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
