d1b35a59e3a54c3feca170efb44c45b4f68a812f
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-06-03 16:06:24 -0300

    Add background image for Hero Component with custom styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './layout/hero/hero.component';
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HeroComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
